a849cdd7f12870c2f3e2d100cb18416768d0a9bb
kirti9510 <kirti21041999@gmail.com>
2021-01-01 10:04:01 +0530

    Heroku

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"kirti21041999@gmail.com\"\n\
+echo "[general]
+email = \"kirti21041999@gmail.com@com\"
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
